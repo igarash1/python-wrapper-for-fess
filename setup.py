25db12de0702ac44d650d3e7d7b3aa7f3e2772bc
igarashi <igarashi.kouki@gmail.com>
2019-08-17 13:07:39 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyFess',
+      version='1.0',
+      description='Python Fess RESTful API Wrapper',
+      author='Koki Igarashi',
+      author_email='igarashi.kouki@gmail.com',
+      packages=['FessAdmin'],
+     )
